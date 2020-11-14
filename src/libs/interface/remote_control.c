00986db2ee8336e719afff23c22f9b21c428668d
Fabio Battaglia <tabaglio@posteo.net>
2020-11-14 12:24:38 +0100

    Begin defining commands for remote control

--- src/libs/interface/remote_control.c
+++ src/libs/interface/remote_control.c
@@ -15,13 +15,33 @@
 #define RESP_START '['
 #define RESP_END ']'
 
-#define CMD_RESET 'K'
-#define CMD_MODEL 'M'
+#define SIPO_BUF_SIZE 5
+
+#define CMD_RESET 'K' // Reset the board
+#define CMD_MODEL 'M' // Returns model of the board
+
+#define CMD_ADDRESS 'A' // Set the current address for the RAM
+#define CMD_ADRINCR 'I' // Increment the current address by one
+#define CMD_WRITE   'W' // Write data at the current address
+#define CMD_READ    'R' // Read data at current address
+
+#define CMD_ERST    'E' // Enables or disables the internal reset
+#define CMD_IOSW    'S' // Switches SRAM access between internal/external
+#define CMD_RWSW    'X' // Switches SRAM between Write and Read mode (when in internal access)
+
+#define CMD_VIEW    'V' // Returns current state
 
 #define CMD_ERROR "CMD_ERR\n"
 #define RESP_MODEL "[M 01]\n"
 
 static char pkt_buffer[PKT_BUFFER_SIZE];
+
+static uint8_t iosw_state = 0;
+static uint8_t erst_state = 0;
+static uint8_t rwsw_state = 0;
+static uint32_t address = 0;
+
+static uint8_t sipo_buffer[SIPO_BUF_SIZE];
 
 static uint8_t receive_pkt(void);
 
