81b147cdc941e51928140ccb5beaff19a5e16550
Fabio Battaglia <hkzlabnet@gmail.com>
2020-11-19 21:05:04 +0100

    untested code for xmodem

--- src/libs/interface/xmodem.c
+++ src/libs/interface/xmodem.c
@@ -23,26 +23,31 @@ static int16_t last_pkt = -1;
 static uint8_t xmodem_sync(uint8_t tries);
 static uint8_t xmodem_recv_pkt(void);
 static uint8_t xmodem_check_packet(void);
-static void xmodem_upload_packet(void);
+static void xmodem_upload_packet(XMODEM_Dump_Type dtype);
 
 uint8_t xmodem_xfer(XMODEM_Dump_Type dtype) {
+    uint8_t nack_retries = 0xFF;
+
     if(!xmodem_sync(XMODEM_DEFAULT_TRIES)) return 0; // No SYNC, time to exit
     wdt_reset();
-    while(1) {
+    while(nack_retries) {
         if(xmodem_recv_pkt()) {
             switch(packet_buf[0]) {
                 case SOH:
-                    if(!xmodem_check_packet()) uart_putchar(NACK);
+                    if(!xmodem_check_packet()) { uart_putchar(NACK); nack_retries--; }
                     else {
+                        nack_retries = 0xFF;
                         last_pkt = packet_buf[1];
-                        xmodem_upload_packet();
+                        xmodem_upload_packet(dtype);
                         uart_putchar(ACK);
                     }
                     break;
                 case EOT: // Transfer completed
                     return 1;
                 default: // Unknown packet...
-                    return 0;
+                    uart_putchar(NACK);
+                    nack_retries--;
+                    break;
             }
 
         } else uart_putchar(NACK);
@@ -76,17 +81,26 @@ static uint8_t xmodem_check_packet(void) {
     uint16_t calc_crc = ((uint16_t)packet_buf[131]) << 8 | packet_buf[132];
     uint8_t expected_packet = (last_pkt + 1) & 0xFF;
 
-    if(crc != calc_crc) return 0;
-    if(packet_buf[1] != expected_packet) return 0;
+    if(crc != calc_crc) return 0; // Corrupted
+    if(packet_buf[1] != expected_packet) return 0; // Have missed a packet?
 
     return 1;
 }
 
 static uint8_t xmodem_recv_pkt(void) {
-    return 0;
+    uint8_t didx = 0;
+
+    while(didx < XMODEM_PKT_SIZE) {
+        if(uart_charavail()) packet_buf[didx++] = uart_getchar();
+        else _delay_ms(5);
+
+        wdt_reset();
+    }
+
+    return 1;
 }
 
-static void xmodem_upload_packet(void) {
-
+static void xmodem_upload_packet(XMODEM_Dump_Type dtype) {
+    // TODO: Upload the packet to the SRAMs
 }
 
