0bcaf8cd3f7a9788879a92fc23549e5fedd528f2
Fabio Battaglia <hkzlabnet@gmail.com>
2020-12-02 10:15:57 +0100

    remove unised proto

--- src/libs/interface/data_buffer.h
+++ src/libs/interface/data_buffer.h
@@ -8,7 +8,5 @@ extern uint8_t sipo_buffer[SIPO_BUFFER_SIZE];
 
 void clear_sipo_buffer(void);
 void fill_sipo_buffer(uint32_t address, uint16_t data);
-uint8_t *get_sipo_buffer(void);
-
 
 #endif /* _DATA_BUFFER_HEADER_ */
